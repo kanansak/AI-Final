c83800e592907fc0ab15cb21e111f16a0b9a805d
Thoth <kanansak555@gmail.com>
2023-04-05 14:22:47 +0700

    Update AI-Fianl.php

--- AI-Fianl.php
+++ AI-Fianl.php
@@ -10,12 +10,12 @@
 <body>
   <div class="container mt-5">
     <h1>รับข้อความ</h1>
-    <form>
+    <form action="" method="GET">
       <div class="form-group">
         <label for="message">ข้อความ</label>
-        <textarea class="form-control" id="message" rows="3"></textarea>
+        <textarea class="form-control" id="message" name="message" rows="3"></textarea>
       </div>
-      <button type="button" class="btn btn-primary" onclick="submitMessage()">ส่งข้อความ</button>
+      <button type="submit" class="btn btn-primary">ส่งข้อความ</button>
     </form>
   </div>
 
@@ -24,25 +24,10 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 
-  <script>
-    function submitMessage() {
-      var message = document.getElementById("message").value;
-      console.log(message);
-      // send message to API
-      fetch("https://api.aiforthai.in.th/emonews/prediction?text=" + encodeURIComponent(message), {
-        headers: {
-          "Apikey": "etTW2zhw5WLwgAoo2HkfnePopSOP52sJ"
-        }
-      })
-      .then(response => response.json())
-      .then(data => console.log(data))
-      .catch(error => console.error(error));
-    }
-  </script>
-
 <?php
-if(isset($_GET["text"])) {
-    $message = $_GET["text"];
+if(isset($_GET["message"])) {
+    $message = $_GET["message"];
+
     $curl = curl_init();
  
   curl_setopt_array($curl, array(
@@ -58,7 +43,7 @@ if(isset($_GET["text"])) {
     )
   ));
    
-  $response = curl_exec($curl);
+  $EmoNews = curl_exec($curl);
   $err = curl_error($curl);
    
   curl_close($curl);
@@ -66,11 +51,36 @@ if(isset($_GET["text"])) {
   if ($err) {
     echo "cURL Error #:" . $err;
   } else {
-    echo $response;
-    console.log($response);
+    //echo $EmoNews;
+    $json_str = $EmoNews;
+    $obj_EmoNews = json_decode($json_str);
+    echo '<script>console.log(' . json_encode($EmoNews) . ');</script>';
+    echo '<div class="container-fluid">
+            <div class="col-lg-6">
+                <h4>ผลลัพธ์</h4>
+                <div class="card">
+                    <div class="card-body">
+                        <p class="card-text">' . $obj_EmoNews->text . '</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-body">
+                        <p class="card-text">Result:</p>
+                        <ul class="list-group">
+                            <li class="list-group-item">Surprise: ' . $obj_EmoNews->result->surprise  . '</li>
+                            <li class="list-group-item">Neutral: ' . $obj_EmoNews->result->neutral  . '</li>
+                            <li class="list-group-item">Sadness: ' . $obj_EmoNews->result->sadness  . '</li>
+                            <li class="list-group-item">Pleasant: ' . $obj_EmoNews->result->pleasant  . '</li>
+                            <li class="list-group-item">Fear: ' . $obj_EmoNews->result->fear  . '</li>
+                            <li class="list-group-item">Anger: ' . $obj_EmoNews->result->anger  . '</li>
+                            <li class="list-group-item">Joy: ' . $obj_EmoNews->result->joy  . '</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>';
   }
 }
 ?>
-
 </body>
 </html>
